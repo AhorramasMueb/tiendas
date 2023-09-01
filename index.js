a8b8a36e95fe88f6ab73f9f8b689deef2e98e2ca
M4G5 <mags_993@hotmail.com>
2023-09-01 12:51:38 -0600

    subiendo proyecto

---
+++ index.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require('express');
+const Datastore = require('nedb');
+const app = express();
+const port = 3001;
+
+const db = new Datastore({ filename: 'my-database.db', autoload: true });
+
+app.use(express.json());
+
+app.post('/api/addData', (req, res) => {
+  const data = req.body;
+
+  db.insert(data, (err, newDoc) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(newDoc);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
